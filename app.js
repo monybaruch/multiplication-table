bb7bfbc03cd5ccc9a8e86e7a174f152b6980309f
מוני ברוך <monybaruch@gmail.com>
2023-08-24 09:55:38 +0300

    finishing the html and css

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const table = document.querySelector('#my-table');
 let output = '';
 
-for (let i = 1; index < 20; i++) {
+for (let i = 1; i <= 10; i++) {
   output += '<tr>';
-  for (let j = 1; i < 10; j++) {
-    output += '<td>' + i * j + '</td>';
+  for (let j = 1; j <= 20; j++) {
+    if (i === 1 || j === 1) {
+      output += '<th>' + i * j + '</th>';
+    } else {
+      output += '<td>' + i * j + '</td>';
+    }
   }
-  out += '<tr>';
+  output += '<tr>';
 }
+
+table.innerHTML = output;
